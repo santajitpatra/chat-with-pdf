be4179545339a096381e622f9ab41fa14641d653
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-08 00:55:04 +0530

    feat : file upload
    feat : file upload prisma schema
    and if user not login by cookie redirect

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,22 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-
-
-const Dashboard = async () => {
+const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) return redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard/>;
 };
 
-export default Dashboard;
+export default Page;
