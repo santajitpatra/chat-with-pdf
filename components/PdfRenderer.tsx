451896bae65526ddbf74358c240c1ee9334ea0a0
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-11 01:40:47 +0530

    feat : render pdf
    feat : render pdf by using import react-pdf

--- components/PdfRenderer.tsx
+++ components/PdfRenderer.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+'use client';
 
-export const PdfRenderer = () => {
+import { Document, Page, pdfjs } from "react-pdf";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+interface PdfRendererProps {
+  url: string;
+}
+
+export const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
-        
-     
-          </div>
+          title
+        </div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
+        </div>
       </div>
     </div>
   );
-}
+};
