88a4dfb9ff9b011e92d183586c6e0fbc47f2b10b
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-14 03:27:41 +0530

    feat: get File Upload Status
    feat: get File Upload Status for all except
    SUCCESS and pending
    also Text area customise

--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,34 +1,32 @@
-import ChatWrapper from "@/components/chat/ChatWrapper"
-import { PdfRenderer } from "@/components/PdfRenderer"
-import { db } from "@/db"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
-import { notFound, redirect } from "next/navigation"
+import ChatWrapper from "@/components/chat/ChatWrapper";
+import { PdfRenderer } from "@/components/PdfRenderer";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server";
+import { notFound, redirect } from "next/navigation";
 
 interface PageProps {
-    params: {
-        fileid: string
-    }
+  params: {
+    fileid: string;
+  };
 }
 
-const Page = async ({params}: PageProps) => {
-    const {fileid} = params
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params;
 
-    const { getUser } = getKindeServerSession()
-    const user = await getUser()
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
 
-    if (!user || !user.id ) 
-    redirect(`/auth-callback?origin=dashboard/${fileid}`)
+  // make sure the user database call
 
-    // make sure the user database call
-
-const file = await db.file.findFirst({
-  where: {
-    id: fileid,
-    userId: user.id,
-  },
-})
-if (!file)  notFound()
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+  if (!file) notFound();
 
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
@@ -42,11 +40,11 @@ if (!file)  notFound()
         </div>
 
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Page
+export default Page;
---
+++ components/chat/ChatInput.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button } from '../ui/button';
+import { Textarea } from '../ui/textarea';
+import { Send } from 'lucide-react';
+
+interface ChatInputProps {
+  isDisabled?: boolean
+
+}
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  return (
+    <div>
+      <div className="absolute bottom-0 left-0 w-full">
+        <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+          <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+            <div className="relative flex flex-col w-full flex-grow p-4">
+              <div className="relative">
+                <Textarea
+                  rows={1}
+                  maxRows={4}
+                  autoFocus
+                 
+                  placeholder="Enter your question..."
+                  className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+                />
+
+                <Button
+                  disabled={isDisabled}
+                  className="absolute bottom-1.5 right-[8px]"
+                  aria-label="send message"
+              
+                >
+                  <Send className="h-4 w-4" />
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatInput
