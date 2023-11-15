effe69d36ccda09caef50d596398da802acfc504
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-16 04:00:29 +0530

    feat : chat section done
    feat : chat section done
    it has some major problem

--- components/chat/ChatWrapper.tsx
+++ components/chat/ChatWrapper.tsx
@@ -7,6 +7,7 @@ import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import { trpc } from "@/app/_trpc/client";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
+import { ChatContextProvider } from "./ChatContext";
 
 interface ChatWrapperProps {
   fileId: string;
@@ -82,13 +83,15 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         );
 
   return (
-    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-      <div className="flex-1 justify-between flex flex-col mb-28">
-        <Messages />
-      </div>
+    <ChatContextProvider fileId={fileId}>
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 justify-between flex flex-col mb-28">
+          <Messages  fileId={fileId}/>
+        </div>
 
-      <ChatInput />
-    </div>
+        <ChatInput />
+      </div>
+    </ChatContextProvider>
   );
 };
 
