e38627b077ec4a73cdfbd534d68abb0ebb429057
Santajit Patra <business.santajitpatra@gmail.com>
2023-11-18 02:35:36 +0530

    feat : navbar configar don
    feat : navbar configar don

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,51 +7,53 @@ import {
   getKindeServerSession,
 } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
+import UserAccountNav from "./UserAccountNav";
 import MobileNav from "./MobileNav";
 
-const Navbar = () => {
+const Navbar = async () => {
   const { getUser } = getKindeServerSession();
-  const user = getUser();
+  const user = await getUser();
 
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>chat with pdf</span>
+            <span>quill.</span>
           </Link>
 
-          <MobileNav />
+          {/* <MobileNav isAuth={!!user} /> */}
 
           <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Pricing
-              </Link>
-              <LoginLink
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Sign in
-              </LoginLink>
-              <RegisterLink
-                className={buttonVariants({
-                  size: "sm",
-                })}
-              >
-                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
-            </>
-            {/* or */}
-            {/* <>
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: "sm",
+                  })}
+                >
+                  Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
                 <Link
                   href="/dashboard"
                   className={buttonVariants({
@@ -61,7 +63,18 @@ const Navbar = () => {
                 >
                   Dashboard
                 </Link>
-              </> */}
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? "Your Account"
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ""}
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
--- components/UserAccountNav.tsx
+++ components/UserAccountNav.tsx
@@ -1,41 +1,52 @@
+import { getUserSubscriptionPlan } from "@/lib/stripe";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
-import { Button } from './ui/button'
-import { Avatar, AvatarFallback } from './ui/avatar'
-import Image from 'next/image'
-import { Icons } from './Icons'
-import Link from 'next/link'
-import { Gem } from 'lucide-react'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import Image from "next/image";
+import { Icons } from "./Icons";
+import Link from "next/link";
+import { Gem } from "lucide-react";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 interface UserAccountNavProps {
-
+  email: string | undefined;
+  name: string;
+  imageUrl: string;
 }
 
-const UserAccountNav = async () => {
+const UserAccountNav = async ({
+  email,
+  imageUrl,
+  name,
+}: UserAccountNavProps) => {
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="overflow-visible">
         <Button className="rounded-full h-8 w-8 aspect-square bg-slate-400">
           <Avatar className="relative w-8 h-8">
-            <div className="relative aspect-square h-full w-full">
-              <Image
-                fill
-                src="/dashboard-preview.jpg"
-                alt="profile picture"
-                referrerPolicy="no-referrer"
-              />
-            </div>
-            <AvatarFallback>
-              <span className="sr-only">santajit</span>
-              <Icons.user className="h-4 w-4 text-zinc-900" />
-            </AvatarFallback>
+            {imageUrl ? (
+              <div className="relative aspect-square h-full w-full">
+                <Image
+                  fill
+                  src={imageUrl}
+                  alt="profile picture"
+                  referrerPolicy="no-referrer"
+                />
+              </div>
+            ) : (
+              <AvatarFallback>
+                <span className="sr-only">{name}</span>
+                <Icons.user className="h-4 w-4 text-zinc-900" />
+              </AvatarFallback>
+            )}
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
@@ -43,11 +54,12 @@ const UserAccountNav = async () => {
       <DropdownMenuContent className="bg-white" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-0.5 leading-none">
-            <p className="font-medium text-sm text-black">santajit</p>
-            
+            {name && <p className="font-medium text-sm text-black">{name}</p>}
+            {email && (
               <p className="w-[200px] truncate text-xs text-zinc-700">
-                business.santajitpatra@gmail.com
+                {email}
               </p>
+            )}
           </div>
         </div>
 
@@ -58,9 +70,13 @@ const UserAccountNav = async () => {
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
+          {subscriptionPlan?.isSubscribed ? (
+            <Link href="/dashboard/billing">Manage Subscription</Link>
+          ) : (
             <Link href="/pricing">
               Upgrade <Gem className="text-blue-600 h-4 w-4 ml-1.5" />
             </Link>
+          )}
         </DropdownMenuItem>
 
         <DropdownMenuSeparator />
@@ -71,6 +87,6 @@ const UserAccountNav = async () => {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
 
-export default UserAccountNav
+export default UserAccountNav;
